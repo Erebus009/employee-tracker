2fd90143fd3fcca3e328afef8d422c607cf2bbf3
Travis Puryear <travispuryear678@gmail.com>
2021-10-23 13:58:30 -0400

    seeds table made and added to company_db.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 -- SQL table info to insert to allow results return soemthing other then undefined
-
-INSERT INTO department(name)(
+INSERT INTO department(name)
 VALUES
 ('Management'),
 ('Engineer'),
@@ -9,6 +8,21 @@ VALUES
 ('Quality Control'),
 ('DevOPS Management'),
 ('Accounting');
-)
 
+INSERT INTO roles(title, salary, department_id)
+VALUES
+('Manager', 100000, 1),
+('Full stack developer', 67000, 2),
+('QA tester', 72000, 4),
+('Warehouse Driver', 45000, 3),
+('Accountant', 47000, 6),
+('Tech Lead', 89000, 7);
 
+INSERT INTO employee(first_name, last_name, role_id) 
+VALUES
+('Bob', 'Haggart', 2),
+('Money', 'Joe', 3),
+('Jim', 'Franken', 2),
+('Moose', 'Poe', 3),
+('Hank', 'Hill', 1),
+('Mono', 'Hands', 7);
